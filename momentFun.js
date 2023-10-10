ea39f1eb1f18579bed6a8e5f51ee78ce3cfea3bf
priyankPortpro <priyank.galani@portpro.io>
2023-10-10 17:33:46 +0530

    fab date fix

--- momentFun.js
+++ momentFun.js
@@ -1,31 +1,41 @@
 // Import the Moment.js library (if not already imported)
 const moment = require('moment');
 
+function getDateOfSelectedDay(dayOfWeek) {
+    const today = moment().tz(userTimeZone()).startOf('day');
+    const selectedDay = moment().tz(userTimeZone()).day(dayOfWeek);
+    if (selectedDay.isBefore(today)) {
+        selectedDay.add(7, 'days');
+    }
+    return selectedDay.toISOString();
+}
+
 function getDaysInMonth(month) {
-    const year = moment().year();
-    const firstDayOfMonth = moment({ year, month: month - 1, day: 1 });
+    const firstDayOfMonth = moment({ month: month, day: 1 }).tz(userTimeZone());
     const firstDayOfNextMonth = firstDayOfMonth.clone().add(1, 'months');
     const daysInMonth = firstDayOfNextMonth.diff(firstDayOfMonth, 'days');
     return daysInMonth;
 }
 
 function getUpcomingDateWithMoment(selectedDate) {
-
-    const currentDate = moment();
-    
-    const daysInCurrentMonth = getDaysInMonth((currentDate.month() + 1))
-
+    let monthAdded = false
+    const currentDate = moment().tz(userTimeZone()).startOf('day');
+    const daysInCurrentMonth = getDaysInMonth((currentDate.month()))
     const currentDayDate = currentDate.date()
-
-    if ( currentDayDate > selectedDate) {
+    if (currentDayDate > selectedDate) {
         currentDate.add(1, 'months');
+        monthAdded = true
     }
     if (selectedDate > daysInCurrentMonth) {
         currentDate.add(1, 'months');
+        monthAdded = true
+    }
+    if (monthAdded) {
+        const daysInNextMonth = getDaysInMonth((currentDate.month()))
+        if (selectedDate > daysInNextMonth) {
+            currentDate.add(1, 'months');
+        }
     }
     currentDate.date(selectedDate);
-
-    return currentDate.format("DD-MM-YY");
+    return currentDate.toISOString();
 }
-
-const upcomingDate = getUpcomingDateWithMoment(6);
